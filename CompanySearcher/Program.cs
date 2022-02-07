8f162d8092450b24349acebd9fed800578e42122
Benjamin Erickson <ben.erickson9276@gmail.com>
2022-02-07 14:39:45 -0600

    First session coding. Began adding the finding and breaking up of the data file.

--- CompanySearcher/Program.cs
+++ CompanySearcher/Program.cs
@@ -8,5 +8,61 @@ namespace CompanySearcher
 {
     internal class Program
     {
+        public static void Main(string[] args)
+        {
+            // Arguments: either the full directory to the data file, or the name of the file in the AppData folder
+
+            // Hard-Coding the argument for testing
+            args = new string[] { @"D:\Project Repositories\KeywordSearcher\CompanySearcher\CompanySearcher\data\SampleSearch.txt" };
+
+            bool fileFound = false;
+            string dataDirectory = string.Empty;
+
+            // Check if the argument provides a full directory
+            if (File.Exists(args[0]))
+            {
+                fileFound = true;
+                dataDirectory = args[0];
+            }
+            else
+            {
+                // Check if the appdata folder exists
+                if (!Directory.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "CompanySearcher")))
+                {
+                    fileFound = false;
+
+                    // If the folder doesn't exist, create it and give the user an error
+                    Console.WriteLine("Application folder not found. Creating folder.");
+
+                    Directory.CreateDirectory(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "CompanySearcher"));
+                }
+                else if (File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), args[0])))
+                {
+                    fileFound = true;
+                    dataDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), args[0]);
+                }
+            }
+
+            // If we weren't able to find the data file, let the user know and stop processing.
+            if (!fileFound)
+            {
+                Console.WriteLine("Data file was unable to be located. Cancelling processing.");                
+            }
+            else
+            {
+                string apiKey = string.Empty;
+                string searchEngineKey = string.Empty;
+                List<List<string>> keywordSearches = new List<List<string>>();
+
+                // Break the data file into an array
+                string[] fileContents = File.ReadAllLines(dataDirectory);
+
+                // If the file has less than three lines, throw an error
+                if (fileContents.Length < 3)
+                {
+                    Console.WriteLine("File does not contain any searches.");
+                }
+            }
+        }
     }
 }
